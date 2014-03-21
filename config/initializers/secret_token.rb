59045bae76d19b1bc08d5c06de82e1c7d55e6c44
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-03-21 14:31:38 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StartupAssist::Application.config.secret_key_base = '92c30807a05f4a59bfda93986b9342c6fb3c75f921cfaf93559171a85f3299cc5d2419286e6a37bd7566aed181e192184e4bfd5ca04d448bd493280b80250a10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StartupAssist::Application.config.session_store :cookie_store, key: '_StartupAssist_session'
