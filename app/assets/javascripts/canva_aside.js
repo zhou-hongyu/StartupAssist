86539885a3447d0db446ef6a6464fb1a288e499c
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-03-24 10:11:13 -0400

    Can drag tag now

--- app/assets/javascripts/canva_aside.js
+++ app/assets/javascripts/canva_aside.js
@@ -45,18 +45,61 @@ StartupAssist.drawTags = function(){
   tag_svg.append("g")
          .attr('class', 'new-tag')
          .append("rect")
+           .attr('class', 'draggable')
            .attr("x", 20)
            .attr('y', 10)
            .attr('height', 160)
            .attr('width', 180)
+           .attr('transform', 'matrix(1 0 0 1 0 0)')
+           .attr('onmousedown', 'StartupAssist.selectElement(evt)')
            .style('fill', '#f1c40f');
 
+};
+
+var selectElement = 0,
+    currentX = 0,
+    currentY = 0,
+    currentMatrix = 0,
+    i = 0;
+
+StartupAssist.selectElement = function(evt){
+
+  selectElement = evt.target;
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+  currentMatrix = selectElement.getAttributeNS(null, "transform").slice(7, -1).split(' ');
+  for(i; i < currentMatrix.length; i++ ){
+    currentMatrix[i] = parseFloat(currentMatrix[i]);
+  }
+  selectElement.setAttributeNS(null, "onmousemove", "StartupAssist.moveElement(evt)");
+
+};
+
+StartupAssist.moveElement = function(evt) {
+  dx = evt.clientX - currentX;
+  dy = evt.clientY - currentY;
+  currentMatrix[4] += dx;
+  currentMatrix[5] += dy;
+  newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+
+  selectElement.setAttributeNS(null, "transform", newMatrix);
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+};
+
+StartupAssist.deselectElement = function(evt) {
+  if( selectElement !== 0 ){
+    selectElement.removeAttributeNS(null, "onmousemove");
+    selectElement.removeAttributeNS(null, "onmouseout");
+    selectElement.removeAttributeNS(null, "onmouseup");
+    selectElement = 0;
+  }
 };
 
 StartupAssist.changeColor = function(d){
   var tag_svg = d3.select('#tag-svg'),
       color_array = ["#ecf0f1", "#2ecc71", "#9b59b6", "#e74c3c", "#3498db", "#f1c40f"];
-      tag_svg.selectAll(".new-tag rect")
-           .transition()
-           .style("fill", color_array[d]);
+  tag_svg.selectAll(".new-tag rect")
+         .transition()
+         .style("fill", color_array[d]);
 };
