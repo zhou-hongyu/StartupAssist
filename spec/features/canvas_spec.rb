07b9be941acde9da74400f58f86218c7280ff01b
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-14 19:29:14 -0400

    add feature test of new user session

--- spec/features/canvas_spec.rb
+++ spec/features/canvas_spec.rb
@@ -9,9 +9,16 @@ feature 'Manage a user' do
 
   scenario "a user should be able to sign up" do
     visit new_user_registration_path
-    fill_in 'Email', with: 'joe@gmail.com'
+    fill_in 'Email', with: 'dude@gmail.com'
     fill_in 'Password', with: 'nevermore'
     fill_in 'Password confirmation', with: 'nevermore'
     click_button 'Sign up'
+  end
+
+  scenario "a user should be able to sign in" do
+    visit new_user_session_path
+    fill_in 'Email', with: 'joe@gmail.com'
+    fill_in 'Password', with: 'nevermore'
+    click_button 'Sign in'
   end
 end
