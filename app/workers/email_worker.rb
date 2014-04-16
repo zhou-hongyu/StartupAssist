5f29d5142cd2876fc0d19d15e9187bf341e3e62f
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-16 16:51:49 -0400

    change the mail where it from

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -2,11 +2,11 @@ class EmailWorker
   @queue = :default
   def self.perform(user_id)
     @user = User.find(user_id)
-    Pony.mail(to: @user.email, subject: "Thank you for using Startup Assist!", body: "Welcome to Startup Assist!")
+    Pony.mail(to: @user.email, from: "founders@startupassist.io", subject: "Thank you for using Startup Assist!", body: "Welcome to Startup Assist!")
   end
 
   def self.welcome(user_id)
     @user = User.find(user_id)
-    Pony.mail(to: @user.email, subject: "Welcome Back!", body: "Welcome back! If you have any questions please contact: zhou.hongyu8976@gmail.com")
+    Pony.mail(to: @user.email, from: "founders@startupassist.io", subject: "Welcome Back!", body: "Welcome back! If you have any questions please contact: zhou.hongyu8976@gmail.com")
   end
 end
