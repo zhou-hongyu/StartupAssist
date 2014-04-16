a8871fe36baa3a7ce51d3fc377b00b3ca5981f63
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-16 17:25:28 -0400

    delete queue

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,5 +1,4 @@
 class EmailWorker
-  @queue = :default
   def self.perform(user_id)
     @user = User.find(user_id)
     Pony.mail(to: @user.email, from: "founders@startupassist.io", subject: "Thank you for using Startup Assist!", body: "Welcome to Startup Assist!")
