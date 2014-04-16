fa287f89617da483b4d365eaa1a5591f140e8a2a
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-16 15:32:41 -0400

    add pony

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :canvas
+  after_commit :say_hi, on: :create
+  after_commit :say_hi_again, on: :update
+
+  def say_hi
+    EmailWorker.perform(self).deliver
+  end
+
+  def say_hi_again
+    EmailWorker.welcome(self).deliver
+  end
 end
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -2,6 +2,11 @@ class EmailWorker
   @queue = :default
   def self.perform(user_id)
     @user = User.find(user_id)
-    Pony.mail(to: @user.email, subject: "Thank you for using Startup Assist!", body: "Welcome!")
+    Pony.mail(to: @user.email, subject: "Thank you for using Startup Assist!", body: "Welcome to Startup Assist!")
+  end
+
+  def self.welcome(user_id)
+    @user = User.find(user_id)
+    Pony.mail(to: @user.email, subject: "Welcome Back!", body: "Welcome back! If you have any questions please contact: zhou.hongyu8976@gmail.com")
   end
 end
